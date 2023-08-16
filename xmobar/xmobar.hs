1cda7a119b768dfe4c866e500bc56eef3ead379a
rw <collardmusic@gmail.com>
2023-08-15 21:42:54 -0700

    sway installed

--- xmobar/xmobar.hs
+++ xmobar/xmobar.hs
@@ -4,14 +4,20 @@ config :: Config
 config =
  defaultConfig
   {
-    font = "ShureTechMono Nerd Font 16"
-  , alpha = 200
+    font = "ShureTechMono Nerd Font 13"
+  , bgColor = "#000000"
+  , fgColor = "#ffffff"
+  , alpha = 100
+  , position = TopH 20
   , commands =
     [
       Run XMonadLog
+    , Run $ MultiCpu  [] 10
+    , Run $ MultiCoreTemp [] 10
+    , Run $ Date "%I:%M %a %b %d" "date" 10
     , Run $ Memory ["t", "Mem: <usedratio>%"] 10
     ]
-  , template = "%XMonadLog% }{ %memory%"
+  , template = "%date%   %XMonadLog% }{ %multicoretemp% %multicpu% %memory%"
   }
 
 main :: IO ()
